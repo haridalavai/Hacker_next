0134cee7e17d9e388779094aa3c53386e4219c28
hari_dalavai <dalavai.hari@gmail.com>
2020-09-26 01:47:57 -0800

    bug fixes

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 import Layout from "../coponents/Layout";
 import Title from "../coponents/postTitle";
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   let posts;
   const res = await fetch("https://node-hnapi.herokuapp.com/news?page=1");
   posts = await res.json();
